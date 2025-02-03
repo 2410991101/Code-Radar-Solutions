02524ac92d9c17f92dea3a795bdd6fb1bd94cd56
2410991101 <jaisvin1101.becse24@chitkara.edu.in>
2025-02-03 22:57:42 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -5,16 +5,16 @@ int main() {
     if(ch=='A'){
         printf("Excellent");
     }
-    else if(ch=="B"){
+    else if(ch=='B'){
         printf("Good");
     }
-    else if(ch=="C"){
+    else if(ch=='C'){
         printf("Average");
     }
-    else if(ch=="D"){
+    else if(ch=='D'){
         printf("Below Average");
     }
-    else if(ch=="F"){
+    else if(ch=='F'){
         printf("Fail");
     }
     return 0;
